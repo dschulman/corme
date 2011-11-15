50d2b756085754a58801c865fa469a185ec67638
Daniel Schulman <danieljschulman@gmail.com>
2011-11-14 22:53:45 -0500

    example data from Bland and Altman (1995)

---
+++ R/data.r
@@ -0,0 +1,23 @@
+#' Repeated measurements of pH and PaCO2 for eight subjects.
+#'
+#' A dataset used as an example in several papers dealing with correlation
+#' and panel data.  Contains repeated measurements of two variables for eight
+#' subjects.  The data is unbalanced, ranging from 3--9 observations per subject
+#' (total 47).
+#'
+#' The variables are:
+#' \describe{
+#' \item{patient}{subject identifier}
+#' \item{pH}{intramural pH measurement}
+#' \item{paCO2}{intramural paCO2 measurement}
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name BlandAltman95
+#' @usage BlandAltman95
+#' @format a data frame with 47 rows and 3 variables
+#' @references Bland, J. M. and Altman, D. G. (1995).
+#'   Calculating correlation coefficients with repeated observations:
+#'   Part 1 --- correlation within subjects.  BMJ, 310(446).
+NULL
